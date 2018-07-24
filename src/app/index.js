657f9fefbdedef3f5e9d6500146aa6ac24deba6b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-07-23 19:46:31 -0700

    basically can replace my existing site

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,21 @@
-import '../styles/fonts.scss'
+import '../styles/main.scss'
 
-const DEFAULT_CANVAS_WIDTH = 300
-const DEFAULT_CANVAS_HEIGHT = 150
+const CANVAS_WIDTH = 400
+const CANVAS_HEIGHT = 1200
 const RGBA = 4
-const INTERVAL_DURATION = 300
 const COLOR_RESOLUTION = 256
 const MAX_COLOR_RESOLUTION = 255
 const INDEX_OF_ALPHA = 3
 const ORIGIN = 0.5
 
-const width = DEFAULT_CANVAS_WIDTH
-const height = DEFAULT_CANVAS_HEIGHT
+const width = CANVAS_WIDTH
+const height = CANVAS_HEIGHT
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 const imagedata = context.createImageData(width, height)
 
-setInterval(() => {
-    for (let n = 0; n < width * height * RGBA; n++) {
-        imagedata.data[n] = n % RGBA === INDEX_OF_ALPHA ? MAX_COLOR_RESOLUTION : Math.floor(Math.random() * COLOR_RESOLUTION)
-    }
-    context.putImageData(imagedata, ORIGIN, ORIGIN)
-}, INTERVAL_DURATION)
+for (let n = 0; n < width * height * RGBA; n++) {
+    imagedata.data[n] = n % RGBA === INDEX_OF_ALPHA ? MAX_COLOR_RESOLUTION : Math.floor(Math.random() * COLOR_RESOLUTION)
+}
+context.putImageData(imagedata, ORIGIN, ORIGIN)
